f858f850fff2acbe4b82f477247c208b29d843d8
David Downie <daviddownie@gmail.com>
2020-07-24 14:35:08 -0400

    removed jquery smooth scroll

--- app.js
+++ app.js
@@ -6,20 +6,8 @@ portfolio.hoverMenu = () => {
   });
 };
 
-// portfolio.smoothScroll = () => {
-//   window.scroll({
-//     top: 2500,
-//     left: 0,
-//     behavior: "smooth",
-//   });
-//   document.querySelector("skillsSection").scrollIntoView({
-//     behavior: "smooth",
-//   });
-// };
-
 portfolio.init = () => {
   portfolio.hoverMenu();
-  //   portfolio.smoothScroll();
 };
 
 $(() => {
